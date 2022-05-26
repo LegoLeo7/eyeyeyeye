daf3a3b2511ef6518e330b821ad08fde43b66812
LegoLeo7 <99499098+LegoLeo7@users.noreply.github.com>
2022-05-26 18:51:37 -0400

    Add files via upload

---
+++ =MAIN EYSE.js
@@ -0,0 +1,49 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	
+	
+}
+
+function playSound(){
+	
+}
+
+#myCanvas
+{
+	border-width:10px;
+	background-color: white;
+	boder-style:ridge;
+	border-color;darkviolet;
+
+}
+ 
+
+function new_image()
+{
+    frabic.Image.formURL(BithdayImage.jpg', function(Img) {
+block_image_object = img;
+
+Block_image_object.scaleToWidth(700);
+Block_image_object.scaleToHeight(510);
+block_image_object.set({
+    top:0,
+    left:0
+})
+canvas.add(block_image_object);
+});
+
+    
+}
+
+
+
+<button class="btn btn-warning btn-lg"
+onclick="playsound()"> dear mom</button>
+
+
+function playsound(){
+    x.play();
+}
